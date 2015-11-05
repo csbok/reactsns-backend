71189f11375419d47a8d8c9d0123e713005688b4
csbok <winmain@gmail.com>
2015-11-05 17:52:22 +0900

    cors change for local dev

--- src/main/java/net/finesoft/react/sns/SimpleCORSFilter.java
+++ src/main/java/net/finesoft/react/sns/SimpleCORSFilter.java
@@ -15,7 +15,7 @@ public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://csbok.github.io");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
